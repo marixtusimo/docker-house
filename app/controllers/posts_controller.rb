f1ad625e3fafeb89c2105b238a4d5fad941fdfa9
marixtusimo <kdfnrjdyewhwy@yahoo.co.jp>
2021-06-08 14:08:43 +0900

    物件編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,19 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+           render :edit
+        end
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :station, :price, :access, :floor, :construction, :location, :build, :security, :equipment, :facility, :image).merge(owner_id: current_owner.id)
